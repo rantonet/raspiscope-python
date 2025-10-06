5ae97f0f997cb2b3b9df9f077743640bb95c1544
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2025-10-06 15:39:54 +0200

    -

--- cli.py
+++ cli.py
@@ -3,3 +3,12 @@ Author: Antlampas
 CC BY-SA 4.0
 https://creativecommons.org/licenses/by-sa/4.0/
 """
+
+from module       import Module
+from configLoader import ConfigLoader
+
+class CLI(Module):
+    def __init__(self):
+        configLoader = ConfigLoader(configPath)
+        config       = configLoader.get_config()
+        pass
