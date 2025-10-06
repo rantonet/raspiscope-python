47109222898b814125dd82f8409cac400b727e7c
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2025-10-06 15:11:26 +0200

    -

--- eventManager.py
+++ eventManager.py
@@ -1,3 +1,9 @@
+"""
+Author: Antlampas
+CC BY-SA 4.0
+https://creativecommons.org/licenses/by-sa/4.0/
+"""
+
 import json
 import signal
 import time
---
+++ gui.py
@@ -0,0 +1,5 @@
+"""
+Author: Antlampas
+CC BY-SA 4.0
+https://creativecommons.org/licenses/by-sa/4.0/
+"""
